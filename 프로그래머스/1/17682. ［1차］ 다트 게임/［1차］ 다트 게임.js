72d46709a2022c5b54c79299d8dff52b30ec411b
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-02-22 19:42:53 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,34 @@
+function solution(dartResult) {
+  var answer = 0;
+  let score = 0;
+  let cnt = [];
+
+    //반복문을 통해 다트 스코어를 순회함
+  for (let i = 0; i < dartResult.length; i++) {
+    //dartResult의 i번째 인덱스가 숫자일때에
+    if (!isNaN(dartResult[i])) {
+      //i-1이 1이고 i가 숫자면 10점..
+      //즉 10점인 경우와 그 외의 경우 처리
+      score = Number(dartResult[i - 1]) === 1 ? 10 : Number(dartResult[i]);
+      //보너스 S일때 1제곱 -> 그냥 그에 해당하는 숫자를 cnt배열에 추가
+    } else if (dartResult[i] === "S") {
+      cnt.push(score);
+      //보너스 D일때 -> Math.pow(score , 2) 2제곱
+    } else if (dartResult[i] === "D") {
+      cnt.push(Math.pow(score, 2));
+      //보너스 T일때 -> 위와 동일 (3제곱)
+    } else if (dartResult[i] === "T") {
+      cnt.push(Math.pow(score, 3));
+      //옵션 *일떄 
+    } else if (dartResult[i] === "*") {
+      cnt[cnt.length - 2] = cnt[cnt.length - 2] * 2;
+      cnt[cnt.length - 1] = cnt[cnt.length - 1] * 2;
+      //옵션 #일때 그에 해당하는 숫자를 -1을 곱해준 숫자를 할당
+    } else if (dartResult[i] === "#") {
+      cnt[cnt.length - 1] = -1 * cnt[cnt.length - 1];
+    }
+  }
+  //3개의 점수 합산
+  answer = cnt.reduce((acc, cur) => acc + cur, 0);
+  return answer;
+}
