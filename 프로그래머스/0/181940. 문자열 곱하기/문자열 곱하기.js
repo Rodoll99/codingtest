56fefcb8e8744afe945db46bd607b884985dcd3e
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-02-27 08:11:27 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, k) {
+    // var answer = '';
+    // for(let i= 0; i < k ; i ++){
+    //     answer += my_string;
+    // }
+    // return answer;
+    
+    //k회 반복하는 메서드
+    return my_string.repeat(k) 
+}
