5082ede0c69c7bf653b7c144a9d55c09437e27f9
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-04-26 17:40:33 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 20.38 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_strings, parts) {
+    var answer = '';
+    for(let i=0; i<my_strings.length;i++){
+        answer+= my_strings[i].split('').slice(parts[i][0],parts[i][1]+1).join('')
+        console.log(my_strings[i].split(''))
+    }
+    return answer;
+}
