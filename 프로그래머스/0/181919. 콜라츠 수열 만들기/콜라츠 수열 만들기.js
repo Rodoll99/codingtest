38f49075b42db7364f3c36420d2773442f37c378
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-04-29 23:27:43 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = [n];
+    while ( n > 1 ){
+        if(n % 2 === 0){
+            n /= 2;
+            answer.push(n)
+        }else if(n%2 !== 0){
+            n = 3 * n + 1;
+            answer.push(n)
+        }
+    }
+    return answer;
+}
