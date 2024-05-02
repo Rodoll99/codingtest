8477057c833f8184a9560ccdbd6465c2cc04dfb4
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-05-02 23:06:02 +0900

    [Bronze V] Title: A-B, Time: 148 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,14 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+	console.log(input[0] - input[1])
+    process.exit();
+});
+
