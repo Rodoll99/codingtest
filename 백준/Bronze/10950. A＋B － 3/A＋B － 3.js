59206c36a5030d2b38b721ea1fb2c4e4db44991f
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-05-07 14:08:25 +0900

    [Bronze V] Title: A+B - 3, Time: 136 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,15 @@
+
+let fs = require('fs');
+// trim을 이용해 앞뒤 공백 제거 , 줄바꾼대로 나누어 배열에 담고 그요소를 한번더 띄어쓰기를 기준으로 나누어 배열에 저장
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(a=> a.split(" "));
+// 제출 시 '/dev/stdin'
+//readFileSync로 txt 파일을 읽어와서 toString과 split을 이용하여 배열로 만들어 input으로 저장한 것이기 때문에 나머지는 알아서 
+//유동적으로 수정하여 사용하면 됩니다. Number 메소드를 이용하여 숫자로 변경한다던지..
+for(let i = 1 ; i <input.length; i ++){
+    let A = Number(input[i][0])
+    let B = Number(input[i][1])
+    console.log( A + B );
+}
+
+
+
