6db8eaff89422bd36f79a7a80c444b1e322a4396
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-05-16 16:46:58 +0900

    [Bronze V] Title: 새싹, Time: 132 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,9 @@
+
+let fs = require('fs');
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n").map(a=> a.split(" "));
+console.log(`         ,r\'\"7
+r\`-_   ,\'  ,\/
+ \\. \". L_r\'
+   \`~\\/
+      \|
+      \|`)
