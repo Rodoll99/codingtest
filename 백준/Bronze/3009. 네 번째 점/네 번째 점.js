8ec2097d05cf462b9c5a1ff565bcebb20cc9f4c7
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-06-28 18:15:42 +0900

    [Bronze III] Title: 네 번째 점, Time: 112 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.js
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.js
@@ -1,16 +1,23 @@
 const fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().trim().split("\n").map(a=> a.split(" "));
-var W = [];
-var H = [];
+var W = new Map();
+var H = new Map();
 let x
 let y
 for(let i = 0 ; i < input.length; i++){
-    W.push(input[i][0])
-    H.push(input[i][1])
+    W.set(input[i][0], (W.get(input[i][0]) || 0)+1)
+    H.set(input[i][1], (H.get(input[i][1]) || 0)+1)
 }
-W = W.sort();
-H = H.sort();
-//두번째 요소가 처음과 같은가? 맞다면 마지막 요소가 다른 한 요소이므로 마지막요소 리턴 , 아니라면 처음것 리턴
-x = W[1] === W[0]? W[2] :W[0];
-y = H[1] === H[0]? H[2] :H[0];
-console.log(`${x} ${y}`);
+
+for(let[key,value] of W){
+    if(value == 1){
+        x = key
+    }
+}
+for(let[key,value] of H){
+    if(value == 1){
+        y = key
+    }
+}
+console.log(x,y)
+
