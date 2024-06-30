45d03ed16d726e72c209f543f03c5ef2c80536cb
Rodoll99 <117357510+Rodoll99@users.noreply.github.com>
2024-06-30 16:46:09 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 100 ms, Memory: 9316 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.js
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.js
@@ -3,14 +3,11 @@ const fs = require("fs");
 // 예제.txt 파일을 읽어와서 배열로 변환
 let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
 
-// 유효한 숫자만 남기고 빈 줄이나 잘못된 입력 제거
-input = input.filter(num => !isNaN(num) && num > 0);
-
 // 세 변의 길이를 정렬
 input = input.sort((a, b) => a - b);
 
 // 삼각형의 세 변의 합이 180도인지 확인
-if (input.length === 3 && input.reduce((a, b) => a + b) === 180) {
+if (input.reduce((a, b) => a + b) === 180) {
     if (input[0] === input[1] && input[1] === input[2]) {
         console.log("Equilateral");
     } else if (input[0] === input[1] || input[1] === input[2]) {
